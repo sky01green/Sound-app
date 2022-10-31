4350a2ac5d8fac2eaff5f360c7ed75efbd44b85f
sky01green <107265148+sky01green@users.noreply.github.com>
2022-10-31 09:59:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown',playsound);
+
+function playsound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key =document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    function removeTransition(e){
+        if(e.propertyName !== 'box-shadow') return;
+        this.classList.remove('playing');
+    }
+    const keys = document.querySelector('.key');
+    keys.forEach(key=> key.addEventListener('transitioned',removeTransition));
+
+}
